77b64aa612aadf6d25f7be80ef1a4c7779c17007
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2024-10-02 16:56:44 +0900

    state を追加

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,27 @@ public class EnemyScript : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public Animator animator;
+
     //エネミーの体力
     [SerializeField] int EnemyHP = 120;
 
     //エネミーの攻撃力
-    //[SerializeField] int Enemy_Attack = 30;
+    [SerializeField] int Enemy_Attack = 30;
+
+    //エネミー攻撃フレーム
+    int Enemy_Attack_Frame01 = 120;
+
+    int Enemy_Attack_Frame02 = 240;
+
+    enum EnemyState
+    {
+        Wite,
+        Run,
+        Attack01,
+        Attack02,
+        Death
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +35,27 @@ public class EnemyScript : MonoBehaviour
         EnemyHP = 240;
 
         rb = GetComponent<Rigidbody>();
+
+        EnemyState enemyState = EnemyState.Wite;
+
+        switch (enemyState)
+        {
+            case EnemyState.Wite:
+                EnemyWite();
+                break;
+            case EnemyState.Run:
+                EnemyRun();
+                break;
+            case EnemyState.Attack01:
+                EnemyAttack01();
+                break;
+            case EnemyState.Attack02:
+                EnemyAttack02();
+                break;
+            case EnemyState.Death:
+                EnemyDeath();
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -31,11 +69,55 @@ public class EnemyScript : MonoBehaviour
         if (other.gameObject.tag == "Sword")
         {
             EnemyHP -= 20;
+        }
+    }
 
-            if (EnemyHP <= 0)
-            {
-                Destroy(this.gameObject);
-            }
+    //エネミー待機
+    void EnemyWite()
+    {
+        
+    }
+
+    //エネミー移動
+    void EnemyRun()
+    {
+        
+    }
+    //エネミー攻撃０１
+    void EnemyAttack01()
+    {
+        Enemy_Attack_Frame01--;
+        if (Enemy_Attack_Frame01 <= 0)
+        {
+            animator.SetBool("Enemy_Attack01", true);
+            Enemy_Attack_Frame01 = 2400;
+        }
+        else
+        {
+            animator.SetBool("Enemy_Attack01", false);
+        }
+    }
+    //エネミー攻撃０２
+    void EnemyAttack02()
+    {
+        Enemy_Attack_Frame02--;
+        if (Enemy_Attack_Frame02 <= 0)
+        {
+            animator.SetBool("Enemy_Attack02", true);
+            Enemy_Attack_Frame02 = 3600;
+        }
+        else
+        {
+            animator.SetBool("Enemy_Attack02", false);
+        }
+    }
+
+    //エネミー死亡
+    void EnemyDeath()
+    {
+        if (EnemyHP <= 0)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
