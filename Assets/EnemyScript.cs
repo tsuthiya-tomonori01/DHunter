76fe59102edc714415cfbc9b22820a789b57a4f8
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2024-10-28 13:01:38 +0900

    タイトルシーンの変更、エネミーがプレイヤーの方向に移動するように変更

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -1,13 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.Playables;
-using static UnityEngine.Random;
 
 public class EnemyScript : MonoBehaviour
 {
@@ -33,7 +28,13 @@ public class EnemyScript : MonoBehaviour
     public Rigidbody rb;
     public Animator animator;
     public EnemyState state; //キャラの状態
-    public Transform TargetTransform; //targetの座標
+    public Transform target;
+    // オブジェクトの移動速度を格納する変数
+    public float moveSpeed;
+    // オブジェクトが停止するターゲットオブジェクトとの距離を格納する変数
+    public float stopDistance;
+    // オブジェクトがターゲットに向かって移動を開始する距離を格納する変数
+    public float moveDistance;
 
     // Start is called before the first frame update
     void Start()
@@ -49,7 +50,24 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 変数 targetPos を作成してターゲットオブジェクトの座標を格納
+        Vector3 targetPos = target.position;
+        // 自分自身のY座標を変数 target のY座標に格納
+        //（ターゲットオブジェクトのX、Z座標のみ参照）
+        targetPos.y = transform.position.y;
+        // オブジェクトを変数 targetPos の座標方向に向かせる
+        transform.LookAt(targetPos);
+
+        // 変数 distance を作成してオブジェクトの位置とターゲットオブジェクトの距離を格納
+        float distance = Vector3.Distance(transform.position, target.position);
+        // オブジェクトとターゲットオブジェクトの距離判定
+        // 変数 distance（ターゲットオブジェクトとオブジェクトの距離）が変数 moveDistance の値より小さければ
+        // さらに変数 distance が変数 stopDistance の値よりも大きい場合
+        if (distance < moveDistance && distance > stopDistance)
+        {
+            // 変数 moveSpeed を乗算した速度でオブジェクトを前方向に移動する
+            transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
+        }
     }
 
     //　敵キャラの状態を設定するためのメソッド 
